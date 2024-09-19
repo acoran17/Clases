3b85c74972b54f80d2c629f4a2c6082a1c9cfdf0
Acoran17 <cristo27603@gmail.com>
2024-09-19 16:21:53 +0100

    Nuevo

--- DEW/Estructuras de control/Ejercicio1.js
+++
@@ -1,6 +0,0 @@
-let edad = parseInt(prompt("Que edad tienes?"));
-if (edad < 18) {
-    alert("No puedes conducir");
-}else {
-    alert("Puedes conducir");
-}
--- DEW/Estructuras de control/Ejercicio4.js
+++
@@ -1,6 +0,0 @@
-let num1 = parseInt(prompt("Dime un numero"));
-if (num1 % 2 == 0) {
-    alert("El numero es par");
-}else {
-    alert("El numero no es par");
-}
